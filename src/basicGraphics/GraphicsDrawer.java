6b2ba50c947ad8141c6524a530c7ef686ae28b07
Nils Nahooy <nils.nahooy@live.com>
2025-01-29 20:42:38 +0100

    Try to get lines curving nicely

--- src/basicGraphics/GraphicsDrawer.java
+++ src/basicGraphics/GraphicsDrawer.java
@@ -7,6 +7,9 @@ import java.util.List;
 import javax.swing.JFrame;
 import javax.vecmath.Vector2d;
 
+import org.apache.commons.lang3.tuple.MutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+
 import drawables.*;
 
 public class GraphicsDrawer {
@@ -26,13 +29,24 @@ public class GraphicsDrawer {
 	}
 
 	private static List<Drawable> createDrawables() {
-		List<Vector2d> p1 = new ArrayList<Vector2d>();
-		List<Drawable> d = new ArrayList<Drawable>();
+		List<Vector2d> p1 = new ArrayList<Vector2d>(); // Points for straight line p1
+		List<Vector2d> p2 = new ArrayList<Vector2d>(); // Points for curved line p2
+		List<Pair<Vector2d, Vector2d>> h2 = new ArrayList<Pair<Vector2d, Vector2d>>(); // Bezier handles for line p2, h2
+		List<Drawable> d = new ArrayList<Drawable>(); // List of Drawables to be sent to the GraphicsCanvas gc.
 
+		//Points in a straight line
 		p1.add(new Vector2d(0,95));
 		p1.add(new Vector2d(600,95));
 		p1.add(new Vector2d(600,0));
-		p1.add(new Vector2d(600,190));
+		
+		//Points in a straight line, to be curved
+		p2.add(new Vector2d(0,75));
+		p2.add(new Vector2d(400,75));
+		
+		
+		//Handles for p2's curves, needs one per point in the Line object.
+		h2.add(new MutablePair<Vector2d,Vector2d>(new Vector2d(-17, 35),new Vector2d(200, 30)));
+		//h2.add(new MutablePair<Vector2d,Vector2d>(new Vector2d(230, 35),new Vector2d(666, 130)));
 
 		d.add(new Cloud(10, 15, 30, Color.YELLOW));
 		d.add(new Cloud(100, 10, 35, Color.GREEN));
@@ -41,6 +55,7 @@ public class GraphicsDrawer {
 		d.add(new Ellipse(190, 100, 80, 80, Color.YELLOW));
 		d.add(new Ellipse(280, 100, 80, 80,true, Color.YELLOW));
 		d.add(new Line(p1, 5, Color.RED));
+		d.add(new Line(p2, h2, 3, Color.DARK_GRAY));
 
 		return d;
 
--- src/drawables/Line.java
+++ src/drawables/Line.java
@@ -9,16 +9,18 @@ import java.util.List;
 
 import javax.vecmath.Vector2d;
 
+import org.apache.commons.lang3.tuple.Pair;
+
 public class Line extends Drawable {
 	private Path2D.Double p1;
-	private List<Vector2d> bezierHandles = new ArrayList<Vector2d>();
+	private List<Pair<Vector2d, Vector2d>> bezierHandles = new ArrayList<Pair<Vector2d, Vector2d>>();
 
 	public Line(List<Vector2d> points, double size, Color color) {
 		super(points, size, color);
 		setPath();
 	}
 
-	public Line(List<Vector2d> points, List<Vector2d> curveHandles, double size, Color color) {
+	public Line(List<Vector2d> points, List<Pair<Vector2d, Vector2d>> curveHandles, double size, Color color) {
 		super(points, size, color);
 		bezierHandles = curveHandles;
 		setPath();
@@ -31,11 +33,16 @@ public class Line extends Drawable {
 		}
 		p1 = new Path2D.Double();
 		p1.moveTo(points.get(0).x, points.get(0).y);
-		for (int i = 1; i < points.size(); i++) {
-			if(bezierHandles.isEmpty()) {
+		if (bezierHandles.isEmpty()) {
+			for (int i = 1; i < points.size(); i++) {
 				p1.lineTo(points.get(i).x, points.get(i).y);
-			} else {
-				//p1.curveTo(bezierHandles.get(i).x, , i, i, i, i);
+			}
+		} else {
+			for (int i = 0; i < points.size(); i++) {
+				int j = i > 1 && i % 2 == 0 ? i-1 : i;
+				Vector2d h1 = bezierHandles.get(j).getLeft();
+				Vector2d h2 = bezierHandles.get(j).getRight();
+				p1.curveTo(h1.x, h1.y, h2.x, h2.y, points.get(i).x, points.get(i).y);
 			}
 		}
 	}
